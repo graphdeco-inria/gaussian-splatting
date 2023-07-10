6a50784106739ad8424e5ad1c6eb422a58b250e8
bkerbl <bkerbl@ad.inria.fr>
2023-07-10 14:40:51 +0200

    Better camera model error message

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -92,7 +92,7 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
             FovY = focal2fov(focal_length_y, height)
             FovX = focal2fov(focal_length_x, width)
         else:
-            assert False, "Colmap camera model not handled!"
+            assert False, "Colmap camera model not handled: only undistorted datasets (PINHOLE or SIMPLE_PINHOLE cameras) supported!"
 
         image_path = os.path.join(images_folder, os.path.basename(extr.name))
         image_name = os.path.basename(image_path).split(".")[0]
