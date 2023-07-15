ab6d668c51ac6defade2d9c82b92b17cf587413b
bkerbl <bkerbl@ad.inria.fr>
2023-07-15 11:12:25 +0200

    Rolling back fixed spatial lr

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -122,7 +122,7 @@ class GaussianModel:
             self.active_sh_degree += 1
 
     def create_from_pcd(self, pcd : BasicPointCloud, spatial_lr_scale : float):
-        self.spatial_lr_scale = 5
+        self.spatial_lr_scale = spatial_lr_scale
         fused_point_cloud = torch.tensor(np.asarray(pcd.points)).float().cuda()
         fused_color = RGB2SH(torch.tensor(np.asarray(pcd.colors)).float().cuda())
         features = torch.zeros((fused_color.shape[0], 3, (self.max_sh_degree + 1) ** 2)).float().cuda()
