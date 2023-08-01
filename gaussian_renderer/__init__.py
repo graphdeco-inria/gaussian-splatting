c10c5ec9c0c0461f1708c62b016719d53fee7bbd
JonathonLuiten <jonoluiten@gmail.com>
2023-08-01 00:34:00 -0400

    Typo in gaussian_renderer.__init__.py
    fixed a typo

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -69,7 +69,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
     # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
     shs = None
     colors_precomp = None
-    if colors_precomp is None:
+    if override_color is None:
         if pipe.convert_SHs_python:
             shs_view = pc.get_features.transpose(1, 2).view(-1, 3, (pc.max_sh_degree+1)**2)
             dir_pp = (pc.get_xyz - viewpoint_camera.camera_center.repeat(pc.get_features.shape[0], 1))
