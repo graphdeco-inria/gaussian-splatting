d98e45952b2aba6fdb12c894cf6ba92bf19bb6b1
zhangmaoxiansheng <455616441@qq.com>
2023-08-23 12:20:48 +0000

    no ip

--- train.py
+++ train.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
     safe_state(args.quiet)
 
     # Start GUI server, configure and run training
-    network_gui.init(args.ip, args.port)
+    #network_gui.init(args.ip, args.port)
     torch.autograd.set_detect_anomaly(args.detect_anomaly)
     training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from)
 
