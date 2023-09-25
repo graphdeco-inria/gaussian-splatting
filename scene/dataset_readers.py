16c5c91fbf63c2dba79afb73aaa6302bc8590e04
Jeff <jeff999955@gmail.com>
2023-09-24 22:33:02 -0700

    Fine tune image loading in Pillow

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -96,11 +96,11 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
 
         image_path = os.path.join(images_folder, os.path.basename(extr.name))
         image_name = os.path.basename(image_path).split(".")[0]
-        image = Image.open(image_path)
-
-        cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
+        with Image.open(image_path) as image:
+            image.load()
+            cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
                               image_path=image_path, image_name=image_name, width=width, height=height)
-        cam_infos.append(cam_info)
+            cam_infos.append(cam_info)
     sys.stdout.write('\n')
     return cam_infos
 
@@ -151,6 +151,9 @@ def readColmapSceneInfo(path, images, eval, llffhold=8):
     else:
         train_cam_infos = cam_infos
         test_cam_infos = []
+
+    print("Train images: ", len(train_cam_infos))
+    print("Test  images: ", len(test_cam_infos))
 
     nerf_normalization = getNerfppNorm(train_cam_infos)
 
