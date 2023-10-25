e9bb1000c640793670b28b144975d22055f0c6cf
Ammar Qammaz <ammarkov@gmail.com>
2023-10-25 21:05:06 +0300

    add a check..

--- docker/run.sh
+++ docker/run.sh
@@ -9,10 +9,18 @@ cd ..
 mkdir -p $1-data/input
 mkdir -p $1-data/output
 
-ffmpeg -i $1 -qscale:v 1 -qmin 1 -vf fps=5 $1-data/input/%04d.jpg
+FPS="5" #<-change this to change framerate
+
+
+if [ -f $1-data/input/0000.jpg ]
+then
+  echo "File $1 appears to have already been split .."
+else
+  ffmpeg -i $1 -qscale:v 1 -qmin 1 -vf fps=$FPS $1-data/input/%04d.jpg
+fi
 
 
 python3.10 convert.py -s $1-data/ --no_gpu
-python3.10  train.py -s $1-data/ -r 1 --model_path=$1-data/output/
+python3.10 train.py -s $1-data/ -r 1 --model_path=$1-data/output/
 
 exit 0
