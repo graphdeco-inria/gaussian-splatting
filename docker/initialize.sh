e16749ab9b7ae30af40346249bc5f875b630db99
Ammar Qammaz <ammarkov@ics.forth.gr>
2023-10-25 18:57:21 +0300

    ....

--- docker/initialize.sh
+++ docker/initialize.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 
+#git clone https://github.com/NVIDIA/cuda-samples
+#cd cuda-samples
+
+
 git clone https://ceres-solver.googlesource.com/ceres-solver
 cd ceres-solver
 git checkout $(git describe --tags) # Checkout the latest release
 mkdir build
 cd build
-cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_CUDA_ARCHITECTURES=native
+cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DUSE_CUDA=OFF #-DCMAKE_CUDA_ARCHITECTURES=native
 make
 sudo make install
 cd ..
@@ -18,7 +22,7 @@ cd colmap
 git checkout dev
 mkdir build
 cd build
-cmake .. -DCMAKE_CUDA_ARCHITECTURES=native
+cmake .. -DCMAKE_CUDA_ARCHITECTURES=native -DCUDA_ENABLED=OFF 
 make
 sudo make install
 CC=/usr/bin/gcc-6 CXX=/usr/bin/g++-6 cmake ..
@@ -28,7 +32,7 @@ cd ..
 
 pip install -q plyfile 
 
-pip install -q https://huggingface.co/camenduru/gaussian-splatting/resolve/main/diff_gaussian_rasterization-0.0.0-cp310-cp310-linux_x86_64.whl
-pip install -q https://huggingface.co/camenduru/gaussian-splatting/resolve/main/simple_knn-0.0.0-cp310-cp310-linux_x86_64.whl
+python3.10 -m pip install -q https://huggingface.co/camenduru/gaussian-splatting/resolve/main/diff_gaussian_rasterization-0.0.0-cp310-cp310-linux_x86_64.whl
+python3.10 -m pip install -q https://huggingface.co/camenduru/gaussian-splatting/resolve/main/simple_knn-0.0.0-cp310-cp310-linux_x86_64.whl
 
 exit 0
---
+++ docker/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+
+cd ..
+
+mkdir -p $1-data/input
+mkdir -p $1-data/output
+
+ffmpeg -i $1 -qscale:v 1 -qmin 1 -vf fps={fps} $1-data/input/%04d.jpg
+
+
+python3.10 convert.py -s $1-data/ --no_gpu
+python3.10  train.py -s $1-data/ -r 1 --model_path=$1-data/output/
+
+exit 0
