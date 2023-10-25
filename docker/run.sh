d09b7c2d8ab5cfea0d04e8d997493b0118a699fe
Ammar Qammaz <ammarkov@gmail.com>
2023-10-25 21:02:17 +0300

    set a framerate..

--- docker/run.sh
+++ docker/run.sh
@@ -9,7 +9,7 @@ cd ..
 mkdir -p $1-data/input
 mkdir -p $1-data/output
 
-ffmpeg -i $1 -qscale:v 1 -qmin 1 -vf fps={fps} $1-data/input/%04d.jpg
+ffmpeg -i $1 -qscale:v 1 -qmin 1 -vf fps=5 $1-data/input/%04d.jpg
 
 
 python3.10 convert.py -s $1-data/ --no_gpu
