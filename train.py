25b3cb8cc95fca3b194928798c4885f664b23253
xvdp <xvpdahlen@gmail.com>
2023-10-31 05:11:09 -0700

    save checkpoints by default

--- train.py
+++ train.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
     parser.add_argument("--test_iterations", nargs="+", type=int, default=[7_000, 30_000])
     parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 30_000])
     parser.add_argument("--quiet", action="store_true")
-    parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
+    parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[7_000, 30_000])
     parser.add_argument("--start_checkpoint", type=str, default = None)
     parser.add_argument('--cuda_blocking', action='store_true', default=True)
     args = parser.parse_args(sys.argv[1:])
