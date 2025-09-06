1b4d3be268079727b55eb91aa72d32dc1bf09389
GalkinDenis <72812069+GalkinDenis@users.noreply.github.com>
2025-09-06 14:54:13 +0300

    Update densify_and_prune function
    I have been little bit exploring the sources and noticed this redundant initialization :)

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -463,7 +463,6 @@ class GaussianModel:
             big_points_ws = self.get_scaling.max(dim=1).values > 0.1 * extent
             prune_mask = torch.logical_or(torch.logical_or(prune_mask, big_points_vs), big_points_ws)
         self.prune_points(prune_mask)
-        tmp_radii = self.tmp_radii
         self.tmp_radii = None
 
         torch.cuda.empty_cache()
