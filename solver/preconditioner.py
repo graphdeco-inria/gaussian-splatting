6c5ebbb551d68740ca7d064741debd504188d416
rogerhh <rogerhh@umich.edu>
2025-11-11 22:53:18 -0800

    fix preconditioner to follow adahessian correctly

--- solver/preconditioner.py
+++ solver/preconditioner.py
@@ -19,26 +19,36 @@ class AdaHessianPreconditioner:
 
         self.D_sq = 0 
 
+        self.S = None
+        self.damp = None
+
+    def set_rescale_and_damp(self, S, damp):
+        self.S = S
+        self.damp = damp
+
     def reset(self):
         self.iteration = 0
         self.D_sq = 0
+        self.S = None
+        self.damp = None
 
     def update(self, Hz_func, cam_provider, scale, num_iter):
         self.iteration += 1
-        D_accum = 0
+        D_sq_accum = 0
         for _ in range(num_iter):
             cam_provider.sample_new()
             vcs = cam_provider.get_cur_batch()
             z = self.z_gen_func()
             Di = z * Hz_func(z, viewpoint_cams=vcs, scale=scale)
-            # D_accum = Di * Di + D_accum
-            D_accum = Di + D_accum
+            D_sq_accum = Di * Di + D_sq_accum
+            # D_accum = Di + D_accum
             del Di, z
 
-        D_accum = D_accum / num_iter
-        D_accum = (D_accum * D_accum).sqrt()
+        D_sq_accum = D_sq_accum / num_iter
+        # D_accum = D_accum / num_iter
+        # D_accum = (D_accum * D_accum).sqrt()
         # D_accum.block_average_and_expand()
-        self.D_sq = ((1 - self.beta2) / num_iter) * D_accum + self.beta2 * self.D_sq
+        self.D_sq = (1 - self.beta2) * D_sq_accum + self.beta2 * self.D_sq
 
     @property
     def D_corrected(self):
